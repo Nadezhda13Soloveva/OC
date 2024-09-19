2e8f19f97f2c486f37683007564b68a88498c465
Nadezhda <nadezhda13soloveva@mail.ru>
2024-09-19 11:13:54 +0300

    Add header files

---
+++ lab1/include/utils.h
@@ -0,0 +1,17 @@
+#ifndef INC_1_UTILS_H
+#define INC_1_UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+enum PipeEnd {
+    READ_END,
+    WRITE_END
+};
+
+void CreatePipe(int pipeFd[2]);
+char* ReadString(FILE* stream);
+
+#endif //INC_1_UTILS_H
